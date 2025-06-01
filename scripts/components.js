082625c313b608ca0084e71e10e875d6e5199fe9
SayeemZaman3 <sayeem312@gmail.com>
2025-06-02 00:41:54 +0600

    fix icon

--- scripts/components.js
+++ scripts/components.js
@@ -5,7 +5,7 @@ class myHeader extends HTMLElement{
         this.innerHTML = `
         <header>
             <div id="mnav">
-                <button id="m-btn"></button>
+                <button id="m-btn"><i class="fi fi-br-menu-burger"></i></button>
                 <div id="mnavBar">
                     <h4>Navigation</h4>
                     <nav></nav>
