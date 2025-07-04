e887d025cd9a9b83fef197d7aadd6415941645cf
SayeemZaman3 <sayeem312@gmail.com>
2025-07-04 21:34:46 +0600

    remove nav js, css

--- scripts/base.js
+++ scripts/base.js
@@ -1,34 +1,3 @@
-// Mobile Navigation
-
-const mnavBtn = $('#m-btn');
-const mnav = $('#mnavBar');
-
-mnavBtn.click(()=>{
-    mnav.toggleClass('active');
-});
-
-$(document).click(e=>{
-    if(!$(e.target).closest('#m-btn, #mnavBar').length) {
-         mnav.removeClass('active');
-    }
-});
-
-// Links
-const links = [
-    { name: 'Home', url: 'home.html' },
-    { name: 'Browse', url: 'browse.html' },
-    { name: 'Contact', url: 'contact.html' }
-]
-
-links.forEach(link => {
-    let li = $('<button>', {
-        text: link.name,
-    }).click(() => window.location.href = link.url );
-    
-    $('#nav').append(li);
-    $('#mnavBar nav').append(li.clone(true));
-});
-
 // Dark/Light Mode
 $('#dark').click(() => {
     
